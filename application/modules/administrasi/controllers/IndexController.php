6293e449ab3a53ee389088a382b4d843f2bf7374
heavendragon <terra.orion@gmail.com>
2012-10-23 10:02:27 +0700

    Add ZF Struktur

---
+++ application/modules/administrasi/controllers/IndexController.php
@@ -0,0 +1,25 @@
+<?php
+
+class Administrasi_IndexController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function xhrAction()
+    {
+        // action body
+    }
+
+
+}
+
+
+
