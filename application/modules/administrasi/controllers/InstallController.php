56d512c4cd0519d3d85efb891be82adf6c983907
heaven-dragon <poweredby.zendframework@gmail.com>
2012-10-28 06:35:32 +0700

    Add additional plugin library, create 1st user as admin

---
+++ application/modules/administrasi/controllers/InstallController.php
@@ -0,0 +1,64 @@
+<?php
+
+class Administrasi_InstallController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        parent::init();
+        $ajaxContext = $this->_helper->getHelper('AjaxContext');
+        $ajaxContext->addActionContext('xhr', 'html')
+                            ->initContext();
+    }
+
+    public function indexAction()
+    {
+        $user = Doctrine_Core::getTable('Administrasi_Model_User')->findAll();
+        if(count($user)):
+            $this->_redirect('/');
+        else:
+            $form = new Administrasi_Form_AdminUser();
+            $form->setAction('/install/xhr/do/init-user/format/html');
+            $this->view->form = $form;
+        endif;
+        
+        $this->view->headScript()->appendFile($this->view->baseUrl('js/validate/jquery.metadata.js'));
+        $this->view->headScript()->appendFile($this->view->baseUrl('js/validate/jquery.validate.min.js'));
+        $this->view->headScript()->appendFile($this->view->baseUrl('js/form/jquery.form.js'));
+    }
+
+    public function xhrAction()
+    {
+        if(!$this->_request->isXmlHttpRequest()):
+            $this->_redirect('/install');
+        endif;
+        $this->_helper->ViewRenderer->setNoRender(true);
+        $params = $this->_getAllParams();
+        switch($params['do']):
+            case 'init-user':
+                $params['username'] = trim(strtolower($params['username']));
+                $params['namalengkap'] = trim($params['namalengkap']);
+                $params['password'] = $params['password'];
+                $params['role'] = 'administrator';
+                $user = new Administrasi_Model_User();
+                $user->fromArray($params);
+                $user->save();
+
+                $users = Doctrine_Core::getTable('Administrasi_Model_User')->findAll();
+                if(count($users)):
+                    echo Zend_Json::encode(array('confirm' => 'Succesfully added'));
+                else:
+                    echo Zend_Json::encode(array('warning' => 'Failed to install'));
+                endif;
+
+            break;
+            default:
+            break;
+        endswitch;
+    }
+
+
+}
+
+
+
---
+++ application/modules/administrasi/forms/AdminUser.php
@@ -0,0 +1,41 @@
+<?php
+
+class Administrasi_Form_AdminUser extends EasyBib_Form
+{
+
+    public function init()
+    {
+        $this->setAttribs(array('class' => 'form-horizontal', 'id'=>'init-user'));
+
+        $this->addElement('text', 'username', array(
+            'label' => 'Username',
+            'required' => true,
+            'attribs' => array('class' => 'required unique',
+                               'placeholder' => 'Tulis Username')));
+        $this->addElement('text', 'namalengkap', array(
+            'label' => 'Nama Lengkap',
+            'required' => true,
+            'attribs' => array('class' => 'required', 'placeholder' => 'Tulis Nama lengkap')));
+
+        $this->addElement('password', 'password', array(
+            'label' => 'Password',
+            'required' => true,
+            'attribs' => array('class' => 'required', 'minlength' => 6, 'placeholder' => 'Tulis Password')));
+
+        $this->addElement('password', 'confirm', array(
+            'label' => 'Confirm Password',
+            'attribs' => array('class' => 'required', 'minlength' => 6, 'equalTo' => '#password', 'placeholder' => 'Tulis kembali passwordnya')));
+
+        $this->addElement('submit', 'submit', array(
+            'label' => 'Simpan',
+            'attribs' => array('class' => 'btn btn-large btn-default'),
+            'ignore' => true));
+
+        $this->addDisplayGroup(array('username', 'namalengkap', 'password', 'confirm',  'submit'), 'user-data');
+        $this->getDisplayGroup('user-data')->setLegend('Admin User');
+        EasyBib_Form_Decorator::setFormDecorator($this, EasyBib_Form_Decorator::BOOTSTRAP, 'submit');
+    }
+
+
+}
+
