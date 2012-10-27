650c5f3f8d52a3feaf0e2b02b9048aff3dfdca53
heaven-dragon <poweredby.zendframework@gmail.com>
2012-10-28 01:24:52 +0700

    Initial layout

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -21,6 +21,38 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
                 ));
             }
         }
+        
+        protected function _initView()
+    {
+        $view = new Zend_View();
+        $view->doctype('XHTML1_STRICT');
+
+        $view->headTitle('Siramkan')->setSeparator(' - ');
+        $view->headMeta()
+                ->appendHttpEquiv('Expires', gmdate('D, d M Y H:i:s', time( ) + 10800) . ' GMT')
+                ->appendHttpEquiv('Cache-Control', 'must-revalidate')
+                ->appendHttpEquiv('Content-Type', 'application/xhtml+xml; charset=utf-8')
+                ->setName('description', 'dokter')
+                ->setName('keywords', 'pemeriksaan')
+                ->setName('robots', 'index, follow');
+
+        $view->headLink()->headLink(array('rel' => 'favicon', 'href'=> '/favicon.ico','type' => 'image/x-icon'), 'PREPEND');
+
+        $view->headLink()->appendStylesheet($view->baseUrl('css/reset.css'));
+        $view->headLink()->appendStylesheet($view->baseUrl('css/bootstrap.min.css'));
+        $view->headLink()->appendStylesheet($view->baseUrl('css/bootstrap-responsive.min.css'));
+        $view->headLink()->appendStylesheet($view->baseUrl('css/style.css'));
+        
+        $view->addHelperPath('ZendX/JQuery/View/Helper', 'ZendX_JQuery_View_Helper');
+        $view->addHelperPath('EasyBib/View/Helper', 'EasyBib_View_Helper');
+        $view->addHelperPath('Vx/View/Helper', 'Vx_View_Helper');
+
+        $view->JQuery()->setLocalPath($view->baseUrl('js/jquery-1.8.2.min.js'))->enable();
+        
+        $viewRenderer = Zend_Controller_Action_HelperBroker::getStaticHelper('ViewRenderer');
+        $viewRenderer->setView($view);
+        return $view;
+    }
 
 }
 
