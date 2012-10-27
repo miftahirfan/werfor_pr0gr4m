5ac6b19881997fca92db51d778801340a3c50fc7
heaven-dragon <poweredby.zendframework@gmail.com>
2012-10-28 00:58:53 +0700

    Add EasyBib Library - Design Model

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,25 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
+    public function _initModuleLoaders()
+     {
+            $this->bootstrap('Frontcontroller');
 
+            $fc = $this->getResource('Frontcontroller');
+            $modules = $fc->getControllerDirectory();
+
+            foreach ($modules AS $module => $dir) {
+                $moduleName = strtolower($module);
+                $moduleName = str_replace(array('-', '.'), ' ', $moduleName);
+                $moduleName = ucwords($moduleName);
+                $moduleName = str_replace(' ', '', $moduleName);
+
+                $loader = new Zend_Application_Module_Autoloader(array(
+                    'namespace' => $moduleName,
+                    'basePath' => realpath($dir . "/../"),
+                ));
+            }
+        }
 
 }
 
