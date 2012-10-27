1f6022c887299f21d5afccc760d3775d39f1bd6c
heaven-dragon <poweredby.zendframework@gmail.com>
2012-10-28 06:42:12 +0700

    Move error controller to module administrasi

---
+++ application/modules/administrasi/Bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Description of Bootstrap
+ *
+ * @author heaven-dragon
+ */
+class Administrasi_Bootstrap extends Zend_Application_Module_Bootstrap
+{
+    //put your code here
+}
+
